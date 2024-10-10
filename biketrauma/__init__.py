fb892d8b2d3ef263d032ea99d9e354375859939b
e20240001069 <lamia.oulebsir@etu.umontpellier.fr>
2024-10-10 20:27:03 +0200

    Ajout des importations des sous-modules dans __init__.py

--- biketrauma/__init__.py
+++ biketrauma/__init__.py
@@ -1,2 +1,5 @@
+from .io.Load_db import load_data  # Remplace Load_db par la fonction correcte si c'est différent
+from .vis.plot_location import plot_location
+from .preprocess.get_accident import get_accident
 __version__ = "0.0.1"
 
